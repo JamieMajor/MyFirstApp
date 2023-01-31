2e0046a0ffbb463de2fbf2a9100fc86a3c5b17c1
JamieMajor <b1046887@live.tees.ac.uk>
2023-01-31 13:59:23 +0000

    Add colour

--- app/src/main/java/com/example/gittutorial/MainActivity.kt
+++ app/src/main/java/com/example/gittutorial/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.gittutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.gittutorial.ui.theme.GitTutorialTheme
 
@@ -28,7 +30,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello whats up $name!")
+    Surface(color = Color.Yellow) {
+        Text(text = "Hello whats up $name!")
+    }
 }
 
 @Preview(showBackground = true)
